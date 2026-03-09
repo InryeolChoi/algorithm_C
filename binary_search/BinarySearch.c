0bd4dd0bfcdd68d286f333c4bfc4536410ee22bd
InryeolChoi <dlsfuf0316@gmail.com>
2026-03-09 16:28:28 +0900

    update binary_search

---
+++ binary_search/BinarySearch.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Point.h"
+
+Point *BinarySearch(Point pointList[], int size, double target)
+{
+    int left;
+    int right;
+    int mid;
+
+    left = 0;
+    right = size - 1;
+
+    while (left <= right)
+    {
+        mid = (left + right) / 2;
+
+        if (target == pointList[mid].point)
+            return &(pointList[mid]);
+        else if (target > pointList[mid].point)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return NULL;
+}
+
+int comparePoint(const void *_elem1, const void *_elem2)
+{
+    Point *elem1 = (Point *)_elem1;
+    Point *elem2 = (Point *)_elem2;
+
+    if (elem1->point > elem2->point)
+        return 1;
+    else if (elem1->point > elem2->point)
+        return -1;
+    else
+        return 0;
+}
+
+int main(void)
+{
+    int length = sizeof Dataset / sizeof Dataset[10];
+    Point *found = NULL;
+    
+    qsort( (void *)Dataset, length, sizeof(Point), comparePoint );
+
+    found = BinarySearch(Dataset, length, 671.8);
+    printf("found!\n");
+    printf("ID: %d, Point: %s \n", found->id, found->point);
+}
---
+++ binary_search/Point.h
@@ -0,0 +1,11 @@
+typedef struct tagPoint {
+    int     id;
+    double  point;
+}   Point;
+
+Point Dataset[] = {
+    (Point){1, 877.88},
+    (Point){2, 176.23},
+    (Point){3, 365.92},
+    (Point){4, 162.44},
+};
